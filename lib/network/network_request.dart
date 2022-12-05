e2b77a3b149f755f591ee0a90d30a324b098da02
hoangtienbo001 <hoangtienbo001@gmail.com>
2022-12-05 17:30:03 +0700

    cm3

--- lib/network/network_request.dart
+++ lib/network/network_request.dart
@@ -1,14 +1,10 @@
-import 'dart:convert';
-
-import '../model/park.dart';
-
 class NetworkRequest {
   static const String url =
       'https://60142474b538980017568c8e.mockapi.io/parkingDetails';
 
-  static List<Park> parsePark(String responseBody) {
-    var list = json.decode(responseBody) as List<dynamic>;
-    List<Park> parks = list.map((model) => Park.fromJson(model));
-    return parks;
-  }
+  // static List<Park> parsePark(String responseBody) {
+  //   var list = json.decode(responseBody) as List<dynamic>;
+  //   List<Park> parks = list.map((model) => Park.fromJson(model));
+  //   return parks;
+  // }
 }
