0438b022b9f0960a55aa9eba445aeb7c49e96b6a
hoangtienbo001 <hoangtienbo001@gmail.com>
2022-12-07 13:18:34 +0700

    run

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:demo1_flutter/screems/park/park_detail_1.dart';
+import 'package:demo1_flutter/screems/park/park_detail_2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-  // var myCustomObject = ParkingDetailScreem();
-  // developer.log(
-  //   'log me',
-  //   name: 'my.app.category',
-  //   error: jsonEncode(myCustomObject),
+  // DevicePreview(
+  //   enabled: !kReleaseMode,
+  //   builder: ((context) => const MyApp()),
   // );
+  runApp(const MyApp());
+  var myCustomObject = ParkingDetailScreem();
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +17,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        // useInheritedMediaQuery: true,
+        // locale: DevicePreview.locale(context),
+        // builder: DevicePreview.appBuilder,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
